29502deb090740f2061e08a253e3261fd6ebe677
skdmldyd23 <skdmldyd23@naver.com>
2019-11-26 13:41:10 +0900

    asd

--- src/main/java/com/douzone/bit/pathfinder/model/entity/CarTb.java
+++ src/main/java/com/douzone/bit/pathfinder/model/entity/CarTb.java
@@ -1,20 +1,35 @@
 package com.douzone.bit.pathfinder.model.entity;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.douzone.bit.pathfinder.model.entity.BranchTb.BranchTbBuilder;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.Accessors;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
+@Builder
+@ToString(exclude = { "carArea" })
+@Accessors(chain = true)
 public class CarTb {
 
 	@Id
@@ -23,5 +38,10 @@ public class CarTb {
 	private String carName;
 	private Double carFuel;
 	private String carNumber;
-	private LocalDateTime carBuy;
+	
+	private LocalDate carBuy;
+	
+	@ManyToOne
+	@JoinColumn(name = "carArea")
+	private AreaTb carArea;
 }
