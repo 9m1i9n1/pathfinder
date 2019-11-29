9737244bc45e93f51868bcc329a9f8227196033c
LDH <ehd8266@gmail.com>
2019-11-29 16:32:19 +0900

    Update History

--- src/main/java/com/douzone/bit/pathfinder/controller/HistoryController.java
+++ src/main/java/com/douzone/bit/pathfinder/controller/HistoryController.java
@@ -1,7 +1,10 @@
 package com.douzone.bit.pathfinder.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,16 +13,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.douzone.bit.pathfinder.model.network.Header;
+import com.douzone.bit.pathfinder.model.network.response.HistoryResponse;
+import com.douzone.bit.pathfinder.service.HistoryService;
+
 @Controller
 @RequestMapping("/history")
 public class HistoryController {
 
+	@Autowired
+	HistoryService historyService;
+	
 	@GetMapping({ "", "/" })
 	public ModelAndView history(ModelAndView mv, HttpServletRequest request) {
 		
 		System.out.println(request.getParameter("pageName"));
 		mv.setViewName("/history");
 		return mv;
+	}
+	
+	@GetMapping("/gethistory.do")
+	public Header<List<HistoryResponse>> getHistory() {
+		
+		return historyService.readHistory();
 	}
 
 }
--- src/main/java/com/douzone/bit/pathfinder/service/HistoryService.java
+++ src/main/java/com/douzone/bit/pathfinder/service/HistoryService.java
@@ -1,8 +1,43 @@
 package com.douzone.bit.pathfinder.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.douzone.bit.pathfinder.model.entity.mongodb.HistoryTb;
+import com.douzone.bit.pathfinder.model.network.Header;
+import com.douzone.bit.pathfinder.model.network.response.HistoryResponse;
+import com.douzone.bit.pathfinder.repository.mongodb.HistoryRepository;
 
 @Service
 public class HistoryService {
 
+	@Autowired
+	private HistoryRepository historyRepository;
+	
+	public Header<List<HistoryResponse>> readHistory() {
+		
+		List<HistoryTb> historys = historyRepository.findAll();
+		
+		List<HistoryResponse> historyList = historys.stream().map(history -> historyResponse(history))
+				.collect(Collectors.toList());
+		
+		return Header.OK(historyList);
+	}
+	
+	private HistoryResponse historyResponse(HistoryTb history) {
+		
+		HistoryResponse response = HistoryResponse.builder()
+				._id(history.get_id()).regdate(history.getRegdate())
+				.username(history.getUsername()).carname(history.getCarname())
+				.dep(history.getDep()).arvl(history.getArvl())
+				.dist(history.getDist()).fee(history.getFee())
+				.dlvrdate(history.getDlvrdate()).arrivedate(history.getArrivedate())
+				.routes(history.getRoutes())
+				.build();
+		
+		return response;
+	}
 }
