74d152ff9e5631f963652b827e1e63fa6b58061d
LDH <ehd8266@gmail.com>
2019-12-13 11:22:04 +0900

    load Image

--- src/main/java/com/douzone/bit/pathfinder/service/HistoryService.java
+++ src/main/java/com/douzone/bit/pathfinder/service/HistoryService.java
@@ -181,7 +181,8 @@ public class HistoryService {
 		HistoryResponse response = HistoryResponse.builder().id(history.getId())
 				.regdate(history.getRegdate().format(formatter)).username(history.getUsername()).carname(carNumber)
 				.dep(history.getDep()).arvl(history.getArvl()).dist(history.getDist()).fee(history.getFee())
-				.dlvrdate(history.getDlvrdate().format(formatter)).arrivedate(history.getArrivedate().format(formatter))
+				.dlvrdate(history.getDlvrdate().format(formatter))
+				.imgSrc(history.getImgSrc()).arrivedate(history.getArrivedate().format(formatter))
 				.routes(history.getRoutes().toString()).build();
 
 		return response;
